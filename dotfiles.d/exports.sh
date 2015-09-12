5932a004cf69477a5dc4ce2c0a1a4532de6d7c6d
Cade Ekblad-Frank <cade@e-f.me>
2015-09-12 13:57:35 -0600

    Migrate clutter to dotfiles.d

---
+++ dotfiles.d/exports.sh
@@ -0,0 +1,41 @@
+# Make vim the default editor
+export EDITOR='vim'
+
+# Enlightened paths for cd
+export CDPATH=".:$HOME:$CDPATH"
+
+# Larger bash history
+export HISTSIZE=32768
+export HISTFILESIZE=$HISTSIZE
+export HISTCONTROL=ignoredups
+# Turn on timestamps for bash history
+export HISTTIMEFORMAT="%F %T "
+# Make some commands not show up in history
+export HISTIGNORE='ls:cd:cd -:pwd:exit:date:* --help'
+
+# Prefer US English and use UTF-8
+export LANG='en_US.UTF-8'
+export LC_ALL='en_US.UTF-8'
+
+# Shamlessly taken from http://linuxtidbits.wordpress.com/2009/03/23/less-colors-for-man-pages/
+export LESS_TERMCAP_mb=$'\E[01;31m'       # begin blinking
+export LESS_TERMCAP_md=$'\E[01;38;5;74m'  # begin bold
+export LESS_TERMCAP_me=$'\E[0m'           # end mode
+export LESS_TERMCAP_se=$'\E[0m'           # end standout-mode
+export LESS_TERMCAP_so=$'\E[38;5;246m'    # begin standout-mode - info box
+export LESS_TERMCAP_ue=$'\E[0m'           # end underline
+export LESS_TERMCAP_us=$'\E[04;38;5;146m' # begin underline
+
+# Don’t clear the screen after quitting a manual page
+export MANPAGER='less -X'
+
+# Always enable colored `grep` output
+export GREP_OPTIONS='--color=auto'
+
+# Link Homebrew casks in `/Applications` rather than `~/Applications`
+export HOMEBREW_CASK_OPTS='--appdir=/Applications'
+
+# Help Docker find itself
+export DOCKER_HOST='tcp://0.0.0.0:2375'
+
+export GOPATH=~/code/go
