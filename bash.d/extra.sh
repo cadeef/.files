ab0548218f9f725c09470dc332d8b6959b7853c9
Cade Ekblad-Frank <cade@e-f.me>
2020-03-08 18:17:49 -0500

    Lint, add shellcheck directive

--- bash.d/extra.sh
+++ bash.d/extra.sh
@@ -1,3 +1,5 @@
+# shellcheck shell=bash
+
 # Add RVM support
 # Disabled to speed up loading, left for easy re-enabling should I need it in the future
 # [ -d ${HOME}/.rvm ] && source "$HOME/.rvm/scripts/rvm"
