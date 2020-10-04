9f5165eece873b3e8957ae8fb054f821b447a7dc
Cade Ekblad-Frank <cade@e-f.me>
2020-10-04 01:28:44 -0500

    Custom window title if in my code directory; remove a bunch of repetition

--- bash.d/bash_prompt.sh
+++ bash.d/bash_prompt.sh
@@ -1,28 +1,27 @@
 # shellcheck shell=bash
 
-# Ugly hack but consistent enough to keep me sane
-
-# Shell check attempts to be helpful here but it gets in the way.
+# Shellcheck attempts to be helpful here but gets in the way, yes I know it's bad.
 
 if [[ ${OSFAMILY} == 'Darwin' ]]; then
     # shellcheck disable=SC2089
     PROMPT_COMMAND="echo -ne \"\033]0;$1⚡ [${PWD}]\007\""
-    # shellcheck disable=SC2090
-    export PROMPT_COMMAND
     PS1="${WINDOW_TITLE}⚡ [\[$(tput setaf 2)\]\w\[$(tput sgr0)\]]--> "
-    export PS1
 elif [[ $(hostname) =~ vm\.local$ ]]; then
     # shellcheck disable=SC2089
     PROMPT_COMMAND="echo -ne \"\033]0;$1🌵 (${HOSTNAME})[${PWD}]\007\""
-    # shellcheck disable=SC2090
-    export PROMPT_COMMAND
     PS1="🌵 (\H)[\[$(tput setaf 2)\]\w\[$(tput sgr0)\]]--> "
-    export PS1
 else
     # shellcheck disable=SC2089
     PROMPT_COMMAND="echo -ne \"\033]0;$1🎈 (${HOSTNAME})[${PWD}]\007\""
-    # shellcheck disable=SC2090
-    export PROMPT_COMMAND
     PS1="🎈 (\H)[\[$(tput setaf 2)\]\w\[$(tput sgr0)\]]--> "
-    export PS1
 fi
+
+# Override if we are in my code directory
+if [[ ${PWD} =~ ${HOME}/code ]]; then
+    # shellcheck disable=SC2089
+    PROMPT_COMMAND="echo -ne \"\033]0;$1🔪 [${PWD/${HOME}\/code\//}]\007\""
+
+fi
+
+# shellcheck disable=SC2090
+export PS1 PROMPT_COMMAND
