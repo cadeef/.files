89a63bb3741bfc54b8f09c3ac8b8eeb91e840193
Cade Ekblad-Frank <cade@e-f.me>
2020-10-04 19:23:35 -0500

    Install brew if it isn't installed in PATH

--- scripts/post_10-installtools.sh
+++ scripts/post_10-installtools.sh
@@ -4,6 +4,13 @@
 
 set -euo pipefail
 
+# Setup Brew if it isn't installed
+if ! hash brew &> /dev/null; then
+    echo "Installing brew..."
+    # Not safe, blah blah blah
+    bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
+
 # MacOS-specific
 if [[ $(uname) == 'Darwin' ]]; then
     # Get that brew action going to install/upgrade from ~/.Brewfile
