5d9129c67ff65e7fef7d319553932c282debdbca
Cade Ekblad-Frank <cade@twain.hsd1.tx.comcast.net>
2020-12-13 20:10:47 -0600

    Migrate to segmented brewfiles

--- scripts/post_10-installtools.sh
+++ scripts/post_10-installtools.sh
@@ -5,14 +5,16 @@
 set -euo pipefail
 
 # Setup Brew if it isn't installed
-if ! hash brew &> /dev/null; then
-    echo "Installing brew..."
-    # Not safe, blah blah blah
-    bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+if ! hash brew &>/dev/null; then
+	echo "Installing brew..."
+	# Not safe, blah blah blah
+	bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
+
+# Install terminal-specific tools
+brew bundle install --file "${HOME}/.Brewfiles/.Brewfile.terminal"
 
 # MacOS-specific
 if [[ $(uname) == 'Darwin' ]]; then
-    # Get that brew action going to install/upgrade from ~/.Brewfile
-    brew bundle install --global
+	brew bundle install --file "${HOME}/.Brewfiles/.Brewfile.dev"
 fi
