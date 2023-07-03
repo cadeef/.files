d2ac5cafe1c8005bd3e24e767f1df70ef9965ef4
Cade Ekblad-Frank <cade@e-f.me>
2023-07-02 19:06:52 -0500

    * Brefile cleanup-- remove virtualbox, netlify-cli * Add sublime package control settings

--- scripts/post_10-installtools.sh
+++ scripts/post_10-installtools.sh
@@ -6,9 +6,9 @@ set -euo pipefail
 
 # Setup Brew if it isn't installed
 if ! hash brew &>/dev/null; then
-	echo "Installing brew..."
-	# Not safe, blah blah blah
-	bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+    echo "Installing brew..."
+    # Not safe, blah blah blah
+    bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
 # Install terminal-specific tools
@@ -16,5 +16,10 @@ brew bundle install --file "${HOME}/.Brewfiles/.Brewfile.terminal"
 
 # MacOS-specific
 if [[ $(uname) == 'Darwin' ]]; then
-	brew bundle install --file "${HOME}/.Brewfiles/.Brewfile.dev"
+    brew bundle install --file "${HOME}/.Brewfiles/.Brewfile.dev"
+
+    # Install virtualbox if for some reason we are on a non-arm system
+    if [[ $(uname -p != "arm") ]]; then
+        brew install --cask virtualbox
+    fi
 fi
