b4053807ed402274eeca72e98f3a04651c9cc0f3
Cade Ekblad-Frank <cade@e-f.me>
2023-07-19 15:30:11 -0500

    Whoops

--- bash.d/git.sh
+++ bash.d/git.sh
@@ -5,10 +5,10 @@
 git_setup() {
     # $df_* are loaded by sourcing ${HOME}/.files/config in .bash_profile
     # shellcheck disable=SC2154
-    if [[ -n ${df_git_email-} ]] && ! git config --global user.email; then
+    if [[ -n ${df_git_email-} ]] && ! git config --global user.email &>/dev/null; then
         git config --global user.email "${df_git_email}"
     fi
-    if [[ -n ${df_git_user-} ]] && ! git config --global user.name; then
+    if [[ -n ${df_git_user-} ]] && ! git config --global user.name &>/dev/null; then
         git config --global user.name "${df_git_user}"
     fi
 }
