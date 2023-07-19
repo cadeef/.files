a455b26ea8acd3b9be4a824a374a914214a2a40f
Cade Ekblad-Frank <cade@e-f.me>
2023-07-19 14:58:04 -0500

    Make git user settings less brittle

--- bash.d/git.sh
+++ bash.d/git.sh
@@ -5,14 +5,11 @@
 git_setup() {
     # $df_* are loaded by sourcing ${HOME}/.files/config in .bash_profile
     # shellcheck disable=SC2154
-    if [[ -n ${df_git_email-} && -n ${df_git_user-} ]]; then
-        if ! grep -q 'email' "${HOME}/.gitconfig"; then
-            git config --global user.email "${df_git_email}"
-        fi
-        if ! grep -q 'name' "${HOME}/.gitconfig"; then
-            git config --global user.name "${df_git_user}"
-        fi
-
+    if [[ -n ${df_git_email-} ]] && ! git config --global user.email; then
+        git config --global user.email "${df_git_email}"
+    fi
+    if [[ -n ${df_git_user-} ]] && ! git config --global user.name; then
+        git config --global user.name "${df_git_user}"
     fi
 }
 
