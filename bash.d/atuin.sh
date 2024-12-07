cda0bbbbe779cdea962c7f38fcc73ddfe37633b1
Cade Ekblad-Frank <cade@e-f.me>
2024-12-07 01:23:51 -0600

    Start using starship.rs for shell prompt

--- bash.d/atuin.sh
+++ bash.d/atuin.sh
@@ -2,3 +2,12 @@
 
 # Initialize atuin
 eval "$(atuin init bash)"
+
+# Fire starship
+eval "$(starship init bash)"
+
+function set_win_title(){
+    echo -ne "\033]0; $(basename "$PWD") \007"
+}
+# shellcheck disable=SC2034
+starship_precmd_user_func="set_win_title"
--- bash.d/bash_prompt.sh
+++ bash.d/bash_prompt.sh
@@ -1,47 +1,47 @@
 # shellcheck shell=bash
 
-prompt_command() {
-    if [[ ${PWD} =~ ${HOME}/code ]]; then
-        PROMPT_EMOJI="🔪"
-        PROMPT_HOSTNAME=""
-        PROMPT_PWD="${PWD/${HOME}\/code\//}"
-    elif [[ ${OSFAMILY} == 'Darwin' || -n "${WSL_DISTRO_NAME-}" ]]; then
-        PROMPT_EMOJI="⚡"
-        PROMPT_HOSTNAME=""
-        # FIXME: tilde needs to be escaped on macos, probably brew bash version (newer) related.
-        # I'm too lazy to figure out why right now and the logic makes it convenient to override.
-        PROMPT_PWD="${PWD/${HOME}/\~}"
-    elif [[ $(hostname) =~ vm\.local$ ]]; then
-        PROMPT_EMOJI="🌵"
-        PROMPT_HOSTNAME="(${HOSTNAME})"
-        PROMPT_PWD="${PWD/${HOME}/~}"
-    else
-        PROMPT_EMOJI="🎈"
-        PROMPT_HOSTNAME="(${HOSTNAME})"
-        PROMPT_PWD="${PWD/${HOME}/~}"
-    fi
+# prompt_command() {
+#     if [[ ${PWD} =~ ${HOME}/code ]]; then
+#         PROMPT_EMOJI="🔪"
+#         PROMPT_HOSTNAME=""
+#         PROMPT_PWD="${PWD/${HOME}\/code\//}"
+#     elif [[ ${OSFAMILY} == 'Darwin' || -n "${WSL_DISTRO_NAME-}" ]]; then
+#         PROMPT_EMOJI="⚡"
+#         PROMPT_HOSTNAME=""
+#         # FIXME: tilde needs to be escaped on macos, probably brew bash version (newer) related.
+#         # I'm too lazy to figure out why right now and the logic makes it convenient to override.
+#         PROMPT_PWD="${PWD/${HOME}/\~}"
+#     elif [[ $(hostname) =~ vm\.local$ ]]; then
+#         PROMPT_EMOJI="🌵"
+#         PROMPT_HOSTNAME="(${HOSTNAME})"
+#         PROMPT_PWD="${PWD/${HOME}/~}"
+#     else
+#         PROMPT_EMOJI="🎈"
+#         PROMPT_HOSTNAME="(${HOSTNAME})"
+#         PROMPT_PWD="${PWD/${HOME}/~}"
+#     fi
 
-    # Call out running as root
-    if [[ ${LOGNAME} == "root" ]]; then
-        PROMPT_EMOJI="⛔️"
-    fi
+#     # Call out running as root
+#     if [[ ${LOGNAME} == "root" ]]; then
+#         PROMPT_EMOJI="⛔️"
+#     fi
 
-    case "${TERM}" in
-        xterm* | rxvt*)
-            PROMPT_WINDOW_TITLE="\[\033]0;${PROMPT_EMOJI} ${PROMPT_HOSTNAME}${PROMPT_PWD}\007\]"
-            ;;
-        *)
-            PROMPT_WINDOW_TITLE=""
-            ;;
-    esac
+#     case "${TERM}" in
+#         xterm* | rxvt*)
+#             PROMPT_WINDOW_TITLE="\[\033]0;${PROMPT_EMOJI} ${PROMPT_HOSTNAME}${PROMPT_PWD}\007\]"
+#             ;;
+#         *)
+#             PROMPT_WINDOW_TITLE=""
+#             ;;
+#     esac
 
-    PS1="${PROMPT_WINDOW_TITLE}${PROMPT_EMOJI} ${PROMPT_HOSTNAME}[\[$(tput setaf 2)\]${PROMPT_PWD}\[$(tput sgr0)\]]--> "
-}
+#     PS1="${PROMPT_WINDOW_TITLE}${PROMPT_EMOJI} ${PROMPT_HOSTNAME}[\[$(tput setaf 2)\]${PROMPT_PWD}\[$(tput sgr0)\]]--> "
+# }
 
-# Load prompt with precmd from bash-preexec
-precmd_functions+=(prompt_command)
+# # Load prompt with precmd from bash-preexec
+# precmd_functions+=(prompt_command)
 
-prompt_command
+# prompt_command
 
 # Load iterm shell integration (it uses PROMPT for tracking)
 if [[ -f "${HOME}/.iterm2_shell_integration.bash" ]]; then
